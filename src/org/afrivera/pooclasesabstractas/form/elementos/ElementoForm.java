0f1aecaabe910c469dc18c9fea28b66c654a0968
afrivera <andresrfelip@hotmail.com>
2022-09-12 21:02:16 -0500

    commit initial poo abstract class

---
+++ src/org/afrivera/pooclasesabstractas/form/elementos/ElementoForm.java
@@ -0,0 +1,22 @@
+package org.afrivera.pooclasesabstractas.form.elementos;
+
+abstract public class ElementoForm {
+
+    protected String valor;
+    protected String nombre;
+
+    public ElementoForm() {
+    }
+
+    public ElementoForm(String nombre) {
+        this();
+        this.nombre = nombre;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    // así se define la clase abstracta sin llaves
+    abstract public String dibujarHtml();
+}
